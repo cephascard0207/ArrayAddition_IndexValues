2305042c0eea41464802f28beade3c6f58afd22d
Cephas Cardozo <95212186+cephascard0207@users.noreply.github.com>
2023-07-18 10:41:35 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#Array_Addition
+#Array_Indexing used for addition
+#Developed by Cephas Cardozo
+
+#Array Declared
+values = [int(input("Input Number A : ")), int(input("Input Number B : "))]
+#Addition
+add = values[0] + values[1]
+#Output
+print("The answer is " + str(add))
